b3fa45ccef7802d3e0f15355f5dd76a2f70f6a31
Jon McLean <Jon McLean>
2018-05-10 10:05:35 -0700

    Specified bins ans saved histogram

--- scripts/week 7 example.R
+++ scripts/week 7 example.R
@@ -5,7 +5,7 @@ fish_data <- read.csv("data/Gaeta_etal_CLC_data_1.csv")
 #this addsa a category to the table that categorizes fish by size
 library(dplyr)
 fish_data_cat = fish_data %>%
-  mutate(length_cat = ifelse(length > 200, "big", "small")) %>% 
+  mutate(length_cat = ifelse(length > 300, "big", "small")) %>% 
   filter(scalelength > 1)
 
 
@@ -17,5 +17,7 @@ ggplot(fish_data_cat, aes(length, scalelength, color = lakeid)) +
 
 
 #Plot histogram of scale length by fish categorical size
-ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
-  geom_histogram()
+week_7_histogram <- ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
+  geom_histogram(bins = 80)
+
+ggsave("figures/week_7_histogram.pdf")

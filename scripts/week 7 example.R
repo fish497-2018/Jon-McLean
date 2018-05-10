8f57d5e3aea5a993fd634dd85a4167520fed7b0b
Jon McLean <Jon McLean>
2018-05-10 10:09:21 -0700

    Added question numbers in comment

--- scripts/week 7 example.R
+++ scripts/week 7 example.R
@@ -2,6 +2,7 @@
 
 fish_data <- read.csv("data/Gaeta_etal_CLC_data_1.csv")
 
+#2)
 #this addsa a category to the table that categorizes fish by size
 library(dplyr)
 fish_data_cat = fish_data %>%
@@ -15,7 +16,7 @@ ggplot(fish_data_cat, aes(length, scalelength, color = lakeid)) +
          geom_point()
 
 
-
+#6)
 #Plot histogram of scale length by fish categorical size
 week_7_histogram <- ggplot(fish_data_cat, aes(x = scalelength, fill = length_cat)) +
   geom_histogram(bins = 80)
